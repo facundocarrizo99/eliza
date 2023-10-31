cba8e7d07ee458fa0acf618048a710ae9a57bee9
lautymelga <104114814+lautymelga@users.noreply.github.com>
2023-10-31 15:53:05 -0300

    Add files via upload

---
+++ diccionario0.py
@@ -0,0 +1,7 @@
+diccionario = {
+    "sinclave":("ok","no entiendo","continue","por que?", "hableme mas"),
+    "hola":("hola","te escucho","contame","bienvenido")
+    "estoy":("porque estas ?","a que se debe","porque pensas que estas","crees que es normal?")
+    "tengo":("porque tenes?","como crees que lo obtuviste?","desde cuando?")
+    "adios":("nos vemos")
+    }
