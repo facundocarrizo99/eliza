4bcb490656e4e36203ca0bae0595d17c3ed7f92a
Facundo Carrizo <49492465+facundocarrizo99@users.noreply.github.com>
2023-10-31 15:51:30 -0300

    analizador de cadenas
    jeje  :-) by vops1mpl3

---
+++ analizar_cadenas.py
@@ -0,0 +1,26 @@
+import random
+def analizarcadena(d,e):
+    divisiones = e.split()
+    flag=0
+    print(divisiones)
+    for palabra in divisiones:
+        if palabra in d:
+            flag=random.choice(d[palabra])
+            print("true")
+            print(d[palabra])
+    return flag
+        
+    
+    
+    
+    
+    
+
+dicc={"porque":("huevo","Huevo2","HUEVO3","HUEVO4","HUEVO35"),
+      "me":"hola",
+      "estoy":"hola"}
+entrada=str(input("ingrese su inquietud"))
+respuesta=analizarcadena(dicc,entrada)
+print(respuesta)
+"""if respuesta==1:
+    print("Se encontró la palabra")"""
