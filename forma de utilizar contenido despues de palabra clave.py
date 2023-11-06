1219afe825174d1dc15684ec2e0b7cba6e6f8ea4
VOPS1MPL3 <sarachofran@gmail.com>
2023-11-06 10:18:08 -0300

    forma de completar palabras claves

---
+++ forma de utilizar contenido despues de palabra clave.py
@@ -0,0 +1,17 @@
+def obtener_despues_de_porque(cadena):
+    # Busca la palabra "porque" en la cadena
+    indice_porque = cadena.find("porque")
+    
+    # Si se encuentra "porque", guarda todo lo que sigue
+    if indice_porque != -1:
+        resultado = cadena[indice_porque + len("porque"):]
+        return resultado
+    else:
+        return "La cadena no contiene la palabra 'porque'."
+
+# Solicita la entrada al usuario
+entrada = input("Introduce una cadena: ")
+
+# Llama a la función y muestra el resultado
+resultado = obtener_despues_de_porque(entrada)
+print("Lo que sigue a 'porque' en la cadena es:", resultado)
